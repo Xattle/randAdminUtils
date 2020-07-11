31be0a4df8a950764aa6856506b84ec5b2352d7f
xattle <ntgrgr@gmail.com>
2020-07-11 17:59:03 -0500

    Restructured arg handlers

--- FileAudit/AuditFileSystem.py
+++ FileAudit/AuditFileSystem.py
@@ -2,6 +2,13 @@
 import csv, sys, argparse, os, textwrap, subprocess
 from prettytable import PrettyTable
 
+#Default variable starts for later
+scan_things = False
+output_csv = False
+output_html = False
+output_cli = False
+
+
 #Setup argparse stuff
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -18,44 +25,18 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("-i", "--infile", help="Inputfile name. Looks only in local directory if path is not specified.")
 parser.add_argument("-s", "--scanpath", help="Defines an absolute path to scan if not using an infile.")
-parser.add_argument("-c", "--csvoutput", help="Only use with scanpath. Sets the output name apart from the default Permissions.csv")
+parser.add_argument("-c", "--csvoutput", help="Only use with scanpath. Sets the output name apart from the default Permissions.csv for the raw scan data")
 parser.add_argument("-o", "--outfile", help="Outfile name. Can specify .csv or .html as the file type. Defaults to .csv")
 parser.add_argument("-g", "--generate", help="Generates the ps1 file to build the .csv in case it needs to be ran elsewhere. Use with -s and -c as needed.")
 args = parser.parse_args()
 
-#logic variables start here
-output_csv = False
-output_html = False
-output_cli = False
-
-#Arg handling
-if args.infile is None and args.scanpath is None:
-    print("Please enter an infile or a scanpath argument.")
-    ###Interactive mode?
-    #is_interactive = True
-    sys.exit()
-
-if args.outfile is not None:
-    if ".csv" in args.outfile:
-        output_csv = True
-    elif ".html" in args.outfile:
-        output_html = True
-        html_name = args.outfile.replace(".html", "")
-    else:
-        output_cli = True
-else:
-    output_cli = True
-
-if args.csvoutput is not None:
-    csv_output = args.csvoutput
-else:
-    csv_output = ".\Permissions.csv"
-
 #Function Stuff
 def scan_path_ps1():
-
-    if os.path.isfile(csv_output) == True:
-        os.remove(csv_output)
+    if args.csvoutput is None:
+        args.csvoutput = ".\Permissions.csv"
+    args.infile = args.csvoutput
+    if os.path.isfile(args.csvoutput) == True:
+        os.remove(args.csvoutput)
 
     if os.path.isfile(".\GenerateFileAudit.ps1") == True:
         os.remove(".\GenerateFileAudit.ps1")
@@ -65,11 +46,12 @@ def scan_path_ps1():
     subprocess.call('powershell.exe -command Set-ExecutionPolicy RemoteSigned', shell=True)
     subprocess.call('powershell.exe .\GenerateFileAudit.ps1', shell=True)
     os.remove(".\GenerateFileAudit.ps1")
+    print(f"Scan is now finished and raw csv outputted to {args.csvoutput}")
 
 def write_ps1_file():
     f = open("GenerateFileAudit.ps1", "a")
     ps1_contents=textwrap.dedent(f'''\
-        $OutFile = "{csv_output}"
+        $OutFile = "{args.csvoutput}"
         $Header = "Folder Path,IdentityReference,AccessControlType,FileSystemRights,IsInherited,InheritanceFlags,PropagationFlags,"
         Del $OutFile
         Add-Content -Value $Header -Path $OutFile
@@ -94,6 +76,7 @@ def write_ps1_file():
     f.close()
 
 def html_formatter():
+    html_name = args.outfile.replace(".html", "")
     html_output_top=textwrap.dedent(f'''\
         <!DOCTYPE html>
         <html>
@@ -126,16 +109,8 @@ def html_formatter():
 
         <p><strong>Sortable by header.</strong></p>
 
-        <table id="myTable">
-          <tr>
-            <th onclick="sortTable(0)">User Group</th>
-            <th onclick="sortTable(1)">File System Rights</th>
-            <th onclick="sortTable(2)">Folder Path</th>
-            <th onclick="sortTable(3)">Is Inherited</th>
-          </tr>
     ''')
     html_output_postdata=textwrap.dedent('''\
-        </table>
 
         <script>
         function sortTable(n) {
@@ -180,6 +155,8 @@ def html_formatter():
         </html>
 
     ''')
+    if os.path.isfile(args.outfile) == True:
+        os.remove(args.outfile)
 
     f = open(f"{args.outfile}", "a")
     f.write(html_output_top)
@@ -188,19 +165,33 @@ def html_formatter():
     f.write("\n")
     f.write(f"")
 
-    with open(args.infile, 'r') as read_obj:
-        # pass the file object to reader() to get the reader object
-        csv_reader = csv.reader(read_obj)
-        # Iterate over each row in the csv using reader object
+    table = PrettyTable()
+    table.field_names = ["User Group", "File System Rights", "Folder Path", "Is Inherited"]
+    with open(args.infile, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        display_count = 0
         for row in csv_reader:
-            # row variable is a list that represents a row in csv
-            format_html = ', '.join(row)
-            format_html = format_html.replace(", ", "</td><td>")
-            html_output_data = (f"<tr><td>{format_html}</td></tr>")
-            f.write(html_output_data)
+            if row["IsInherited"] == "False":
+                table.add_row([row["IdentityReference"],row["FileSystemRights"],row["Folder Path"],row["IsInherited"]])
+                display_count += 1
+            line_count += 1
+        table.sortby = "User Group"
+        f.write(table.get_html_string())
+
+
     f.write("\n")
     f.write(html_output_postdata)
     f.close
+
+    f1 = open(args.outfile, 'r')
+    f2 = open(args.outfile, 'w')
+    for line in f1:
+        f2.write(line.replace('table', 'table id="myTable"'))
+    f1.close()
+    f2.close()
+
+    print(f"HTML Output has been sent to: {args.outfile}")
 
 def cli_table_display():
     table = PrettyTable()
@@ -217,47 +208,43 @@ def cli_table_display():
         table.sortby = "User Group"
         print(table)
         print()
-        print(f'Inheritance Flag filter is active. Displaying {display_count} out of {line_count} entries.')
+        print(f'Inheritance Flag filter is active by default. Displaying {display_count} out of {line_count} entries.')
+
+##############################
+#Check for args and set bools#
+if args.infile is None and args.scanpath is None:
+    print("Please enter an infile or a scanpath argument.")
+    sys.exit()
+
+if args.infile is not None and args.scanpath is not None:
+    print("Choose infile or scanpath. Not both.")
+    sys.exit()
+
+if args.scanpath is not None:
+    scan_things = True
+
+if args.outfile is not None:
+    if ".csv" in args.outfile:
+        output_csv = True
+    elif ".html" in args.outfile:
+        output_html = True
+    else:
+        Print("Outfile is not specified or not .csv/.html. Outputting to CLI.")
+        output_cli = True
 
 ##############################
 #Call functions and run stuff#
+if scan_things is True:
+    scan_path_ps1()
+
+if output_csv == True:
+    Print("CSV Output not set up!! Displaying CLI Output.")
+    cli_table_display()
+elif output_html == True:
+    html_formatter()
+elif output_cli == True:
+    cli_table_display()
+
 if args.generate is True:
     write_ps1_file()
     print("GenerateFileAudit.ps1 has been created.")
-    sys.exit()
-elif args.infile is not None and args.scanpath is None:
-    #Check for infile being the right type and existing
-    if ".csv" not in args.infile:
-        print()
-        print("Infile is not of type .csv")
-        sys.exit()
-
-    if os.path.isfile(args.infile) == False:
-        print()
-        print("File does not exist: " + args.infile)
-        sys.exit()
-
-    #Say Stuff
-    print ()
-    print ("Infile: " + args.infile)
-    if args.outfile is not None:
-        print ("Outfile: " + args.outfile)
-        if ".csv" in args.outfile:
-            print("CSV OUTPUT")
-        elif ".html" in args.outfile:
-            print("HTML OUTPUT")
-            html_formatter()
-        else:
-            print("Where am I?")
-
-    else:
-        print ("Outfile not specified. Displaying table in cli.")
-    print()
-    cli_table_display()
-    print(f"Scan and charting complete. CSV file has been saved as {csv_output}")
-elif args.scanpath is not None:
-    scan_path_ps1()
-    args.infile = csv_output
-    cli_table_display()
-else:
-    print("How did you find me?")
