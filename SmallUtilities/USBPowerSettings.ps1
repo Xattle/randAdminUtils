ba34bc062bfed8e981a4c215b51a651294b75c28
xattle <ntgrgr@gmail.com>
2022-05-19 09:58:02 -0500

    New usb power script

---
+++ SmallUtilities/USBPowerSettings.ps1
@@ -0,0 +1,19 @@
+# Disable the advanced power saving mode on all USB Hubs 
+# Has to be ran as admin
+# Device Manager, Universal Serial Bus controllers, Select USB Root Hub, Properties, Power Management, deselect "Allow the computer to turn off this device to save power"
+
+$hubs = Get-WmiObject Win32_USBHub
+$powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
+foreach ($p in $powerMgmt)
+{
+  $IN = $p.InstanceName.ToUpper()
+  foreach ($h in $hubs)
+  {
+    $PNPDI = $h.PNPDeviceID
+                if ($IN -like "*$PNPDI*")
+                {
+                    $p.enable = $False
+                    $p.psbase.put()
+                }
+  }
+}
