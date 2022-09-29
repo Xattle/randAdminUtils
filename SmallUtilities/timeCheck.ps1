a89716c7173f9e216df57e0a82b700255ba4a48b
Xattle <ntgrgr@gmail.com>
2022-09-29 09:25:35 -0500

    Formatting and bugfixes

--- SmallUtilities/timeCheck.ps1
+++ SmallUtilities/timeCheck.ps1
@@ -5,14 +5,16 @@ while ($notPressed)
     {
         $notPressed = $false
     }
-    $startTime = Get-Date "08:00am"
+    $startTime = Get-Date "08:30am"
     $curTime = Get-Date
     
-    $secPerTotal = 28800
+    $secPerTotal = 30600
     $secElapsed = ($curTime - $startTime).TotalSeconds
     $percentSpent = (($secElapsed / $secPerTotal) * 100)
-    Write-Host ("`r{0:n2}%" -f $percentSpent) -NoNewline
-    Start-Sleep -Seconds 1
+    $percentSpent = "{0:n2}" -f $percentSpent
+    $percentSpent = [float] $percentSpent
+    Write-Progress -Activity "Day in Progress" -Status "$percentSpent% Complete:" -PercentComplete $percentSpent
+    Start-Sleep -Milliseconds 250
 }
 
 Write-Host
